b90487b83d9c19a3893a2f28f73d2f8a796a785a
peeboon11 <natthaprug7520@gmail.com>
2024-05-12 10:23:25 +0700

    open project

---
+++ lostsagaweb/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+function index() {
+  return (
+    <>
+      <Head>
+        <title>Lost Saga Thailand</title>
+      </Head>
+    </>
+
+  )
+}
+
+export default index
